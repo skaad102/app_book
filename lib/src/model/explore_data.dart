61362806ecc9aa7c8cbc486c0772597e996c3ab6
daniel adame <skaad102@gmail.com>
2022-10-17 15:16:18 -0500

    models

---
+++ lib/src/model/explore_data.dart
@@ -0,0 +1,12 @@
+import 'post.dart';
+import 'explore_recipe.dart';
+
+class ExploreData {
+  final List<ExploreRecipe> todayRecipes;
+  final List<Post> friendPosts;
+
+  ExploreData(
+    this.todayRecipes,
+    this.friendPosts,
+  );
+}
